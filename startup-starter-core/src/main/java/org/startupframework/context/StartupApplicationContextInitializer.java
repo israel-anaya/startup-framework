4793d8fe8b3733805a8d4eeab2aa0ec96493b04c
Jesus Israel Anaya Salazar <anayaj0511@hotmail.com>
2020-09-11 18:57:16 -0500

    clean code

--- startup-starter-core/src/main/java/org/startupframework/context/StartupApplicationContextInitializer.java
+++ startup-starter-core/src/main/java/org/startupframework/context/StartupApplicationContextInitializer.java
@@ -1,15 +1,11 @@
 package org.startupframework.context;
 
 import java.util.Arrays;
-import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import org.springframework.boot.env.YamlPropertySourceLoader;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.core.env.PropertySource;
-import org.springframework.core.io.Resource;
 import org.startupframework.WaitForService;
 import org.startupframework.exception.StartupException;
 
@@ -17,35 +13,17 @@ public class StartupApplicationContextInitializer
 		implements ApplicationContextInitializer<ConfigurableApplicationContext> {
 
 	final static String DEPENDENCIES = "startup.dependencies";
-	
+
 	static boolean readyDependencies = false;
 
 	@Override
 	public void initialize(ConfigurableApplicationContext applicationContext) {
 
-		if(readyDependencies) {
+		if (readyDependencies) {
 			return;
 		}
-		
-		String dependencies = applicationContext.getEnvironment().getProperty(DEPENDENCIES);
-		String[] profiles = applicationContext.getEnvironment().getActiveProfiles();
 
-		if (dependencies == null) {
-			for (String profile : profiles) {
-				try {
-					String resourceName = String.format("classpath:application-%s.yml", profile);
-					Resource resource = applicationContext.getResource(resourceName);
-					YamlPropertySourceLoader sourceLoader = new YamlPropertySourceLoader();
-					List<PropertySource<?>> yamlProperties = sourceLoader.load("startup", resource);
-					for (PropertySource<?> propertySource : yamlProperties) {
-						applicationContext.getEnvironment().getPropertySources().addLast(propertySource);
-					}
-				} catch (Exception e) {
-					continue;
-				}
-			}
-			dependencies = applicationContext.getEnvironment().getProperty(DEPENDENCIES);
-		}
+		String dependencies = applicationContext.getEnvironment().getProperty(DEPENDENCIES);
 
 		if (dependencies != null) {
 			waitForDependencies(dependencies);
