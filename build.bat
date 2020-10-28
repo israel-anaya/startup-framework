11da3eec59ec351e466cf4453f86b42632f712ca
Jesus Israel Anaya Salazar <anayaj0511@hotmail.com>
2020-10-27 20:15:04 -0600

    msa-parent

--- build.bat
+++ build.bat
@@ -4,6 +4,7 @@ echo Samples Startup Framework
 echo -----------------------------------------------------------------
 
 call mvn -f startup-starter-parent/pom.xml clean install
-call mvn -f startup-starter-ms-parent/pom.xml clean install
 call mvn -f startup-starter-core/pom.xml clean install
 call mvn -f startup-starter-data/pom.xml clean install
+call mvn -f startup-starter-ms-parent/pom.xml clean install
+call mvn -f startup-starter-msa-parent/pom.xml clean install
--- clean.bat
+++ clean.bat
@@ -4,8 +4,10 @@ echo Samples Startup Framework
 echo -----------------------------------------------------------------
 
 call mvn -f startup-starter-parent/pom.xml clean
-call mvn -f startup-starter-ms-parent/pom.xml clean
 call mvn -f startup-starter-core/pom.xml clean
 call mvn -f startup-starter-data/pom.xml clean
+call mvn -f startup-starter-ms-parent/pom.xml clean
+call mvn -f startup-starter-msa-parent/pom.xml clean
+
 
 
